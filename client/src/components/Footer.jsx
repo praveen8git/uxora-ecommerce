a5d5b04383ab341f17df70bc831d9403b7215233
praveen8git <omniplayer.cosmo9@gmail.com>
2024-04-09 21:17:41 +0530

    chore: update env variables and footer links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <footer className="container-fluid mt-3 p-0">
@@ -10,21 +10,21 @@ const Footer = () => {
                             <div className="row h-100 ">
                                 <div className="col d-flex flex-column justify-content-between">
                                     <div>
-                                        <a href="#" className="navbar-brand font-color text-uppercase fs-3">ÜXORA</a>
+                                        <Link to="/" className="navbar-brand font-color text-uppercase fs-3">ÜXORA</Link>
                                         <p className="card-text mb-2">
                                             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                                         </p>
                                     </div>
                                     <div className="mt-2">
-                                        <a href="#" className="footer-link text-uppercase text-decoration-none me-2">
+                                        <Link to="#" className="footer-link text-uppercase text-decoration-none me-2">
                                             <i className="ai ai-instagram-logo fs-1"></i>
-                                        </a>
-                                        <a href="#" className="footer-link text-uppercase text-decoration-none me-2">
+                                        </Link>
+                                        <Link to="#" className="footer-link text-uppercase text-decoration-none me-2">
                                             <i className="ai ai-dribbble-logo fs-1"></i>
-                                        </a>
-                                        <a href="#" className="footer-link text-uppercase text-decoration-none me-2">
+                                        </Link>
+                                        <Link to="#" className="footer-link text-uppercase text-decoration-none me-2">
                                             <i className="ai ai-behance-logo fs-1"></i>
-                                        </a>
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
@@ -42,24 +42,24 @@ const Footer = () => {
                                         <div className="col-sm-6 mt-2">
                                             <h2 className="card-heading font-color text-uppercase fs-4">woman</h2>
                                             <ul className="footer-menu p-1">
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">Dresses</a></li>
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">pants</a></li>
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">skirts</a></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/women/dresses">Dresses</Link></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/women/pants">pants</Link></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/women/skirts">skirts</Link></li>
 
                                             </ul>
                                         </div>
                                         <div className="col-sm-6 mt-2">
                                             <h2 className="card-heading font-color text-uppercase fs-4">men</h2>
                                             <ul className="footer-menu p-1">
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">shirts</a></li>
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">pants</a></li>
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">hoodies</a></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/men/shirts">shirts</Link></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/men/pants">pants</Link></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/men/hoodies">hoodies</Link></li>
 
                                             </ul>
                                         </div>
@@ -71,24 +71,24 @@ const Footer = () => {
                                         <div className="col-sm-6 mt-2">
                                             <h2 className="card-heading font-color text-uppercase fs-4">kids</h2>
                                             <ul className="footer-menu p-1">
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">Dresses</a></li>
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">T-shirts</a></li>
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">Hoodies</a></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/kids">Dresses</Link></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/kids">T-shirts</Link></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/kids">Hoodies</Link></li>
 
                                             </ul>
                                         </div>
                                         <div className="col-sm-6 mt-2">
                                             <h2 className="card-heading font-color text-uppercase fs-4">links</h2>
                                             <ul className="footer-menu p-1">
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">home</a></li>
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">login</a></li>
-                                                <li><a className="footer-link font-color text-decoration-none text-uppercase"
-                                                    href="#">contact</a></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/">home</Link></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/login">login</Link></li>
+                                                <li><Link className="footer-link font-color text-decoration-none text-uppercase"
+                                                    to="/contact">contact</Link></li>
 
                                             </ul>
                                         </div>
@@ -106,7 +106,7 @@ const Footer = () => {
                             <div className="row">
                                 <div className="col">
                                     <p className="text-center copyright m-0">
-                                        <a href="#" className="navbar-brand font-color" style={{ fontSize: 1 + 'rem' }}>ÜXORA </a>
+                                        <Link to="/" className="navbar-brand font-color" style={{ fontSize: 1 + 'rem' }}>ÜXORA </Link>
                                         - © Copyright 2024
                                     </p>
                                 </div>
