c4088d78ae4dee694320134fe35b2a6103d012c9
praveen8git <omniplayer.cosmo9@gmail.com>
2024-04-09 22:59:27 +0530

    ops: update cors setting for deployment

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const { PORT } = process.env;
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://uxora-ecommerce.vercel.app'],
     credentials: true
   }
 
